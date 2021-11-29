f7626646efe24d402c9070458ae0193fb4e1b298
Jake Rundall <rundall@illinois.edu>
2021-11-29 17:02:27 -0600

    add init.pp/base class

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+# @summary Configures Lmod for use on an NCSA cluster.
+#
+# This profile installs and configures Lmod for use on an NCSA cluster.
+#
+# @example
+#   include profile_lmod
+#
+class profile_lmod {
+}
